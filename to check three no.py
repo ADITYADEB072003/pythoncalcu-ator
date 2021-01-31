fece75786a29340cc1d493a2de4b694e6e1b4a92
Aditya Deb Chowdhury <63070122+ADITYADEB072003@users.noreply.github.com>
2021-01-31 21:45:08 +0530

    Add files via upload

---
+++ to check three no.py
@@ -0,0 +1,10 @@
+ #to check three no
+a=float(input("enter the first no"))
+b=float(input("enter the second no"))
+c=float(input("enter the third no"))
+if a>b and a>c:
+    print("a")
+elif b>a and b>c:
+    print("b")
+else:
+    print("c")
